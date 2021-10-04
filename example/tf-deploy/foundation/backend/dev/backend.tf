e83eddec9affea7b13631b2794f820024043e828
Andrew Clure <andrew@asc-solutions.co.uk>
2021-10-04 15:01:06 +0100

    updated template files and examples

--- example/tf-deploy/foundation/backend/dev/backend.tf
+++ example/tf-deploy/foundation/backend/dev/backend.tf
@@ -2,9 +2,9 @@
 
 # Backend Storage account details
 #tenant_id       = "" # Tenant ID where terraform remote state Storage Account exists
-subscription_id = "2301e9c5-b046-4805-ae2e-366cb7176d9f" # Subscription ID where the remote state container lives
+subscription_id = "blahblahblah" # Subscription ID where the remote state container lives
 
 resource_group_name  = "rg-devops-prd"   # Resource Group where the Terraform remote state storage account lives.
-storage_account_name = "stimtfstatestoreuks01" # The name of the Storage Account used for terraform remote state.
+storage_account_name = "storageaccountname00001" # The name of the Storage Account used for terraform remote state.
 container_name       = "tfstate"               # Name of the blob container used for the terraform remote state blobs
-key                  = "terraformTest.terraform.tfstate" # Name to assign to the remote state file (blob).
+key                  = "demopipeline.terraform.tfstate" # Name to assign to the remote state file (blob).
--- tf-deploy/environments/test/environment.tfvars
+++
@@ -1,1 +0,0 @@
-rg_name = "rg-test-asc-demo"
